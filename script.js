70094a6a6861593bd162eaac3e03fd462a90fad5
David Miranda <david@storylog.com>
2020-05-08 11:48:29 -0400

    delete people

--- script.js
+++ script.js
@@ -31,6 +31,16 @@ let formElem = document.querySelector("form");
 let personsNameInput = document.querySelector(".persons-name");
 let queueElem = document.querySelector(".queue");
 
+document.addEventListener("click", async function (event) {
+  if (event.target.closest(".delete-person")) {
+    event.preventDefault();
+    let personElem = event.target.closest("[data-id]");
+    let id = personElem.getAttribute("data-id");
+    await removePerson(id);
+    render();
+  }
+});
+
 formElem.addEventListener("submit", async function (event) {
   event.preventDefault();
   let personsName = personsNameInput.value;
@@ -44,10 +54,19 @@ formElem.addEventListener("submit", async function (event) {
 async function render () {
   let peopleArray = await getPeople();
   let peopleHtml = peopleArray.map(person => {
-    return `<div data-id="${person._id}">${person.name}</div>`;
+    return `<div data-id="${person._id}"><a class="delete-person"href="#">X</a> ${person.name}</div>`;
   }).join("");
   
   queueElem.innerHTML = peopleHtml;
 }
 
 render();
+
+
+
+
+
+
+
+
+
